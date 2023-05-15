e1a2de18343f4e6c4a84a42d9211c283c3c31713
RobyParak <677676@student.inholland.nl>
2023-05-15 13:32:35 +0200

    Dash Dash - added search and column division

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -6,18 +6,30 @@
   <div class="q-gutter-y-md column" style="max-width: 300px;">
     <q-input filled v-model="emailInput" label="Username/Email" placeholder="Username/Email" :dense="dense" />
     <q-input v-model="password" filled type="password" label="Password" :dense="dense"/>
-    <q-btn style="background: #f919a9; color: white" label="Login" />
+    <q-btn  class="q-ml-auto" style="background: #f919a9; color: white" label="Login" @click="login" />
   </div>
 </div>
 
   </template>
 
 <script>
+import {QBtn} from "quasar";
 export default {
-  name: "LoginView.vue"
+name: 'LogoutButton',
+    components: {
+  QBtn
+},
+methods: {
+  login() {
+    // perform logout action here
+  }
+}
 }
 </script>
 
 <style scoped>
-
+.q-btn {
+  border-radius: 20px;
+  padding: 8px 16px;
+}
 </style>
