e63771ab69676f6b2989f8836da3068991908785
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-05-21 14:30:52 +0200

    deleted new user page

--- src/components/EmployeeDashboard.vue
+++ src/components/EmployeeDashboard.vue
@@ -7,7 +7,6 @@
         inline
         :options="[
           { label: 'Edit User/Bank information', value: 'edit' },
-          { label: 'New User', value: 'newUser' },
           { label: 'New Bank Account', value: 'newBankAccount' },
           { label: 'Delete User', value: 'delUser' },
           { label: 'Deactivate Bank Account', value: 'delAccount' }
@@ -127,67 +126,7 @@
             </template>
           </q-table>
         </q-tab-panel>
-<!-- ADD A USER -->
-        <q-tab-panel name="newUser">
-          <div class="text-h6">Add New User</div>
-          <div class="q-pa-md" style="justify-content: center; padding-left: 3em; max-width: 80%;">
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input
-            filled
-            v-model="firstName"
-            label="First Name"
-            placeholder="First Name"
-            lazy-rules
-            :rules="[val => val && val.length > 0 || 'Please enter your first name']"
-        />
-        <q-input
-            filled
-            v-model="lastName"
-            label="Last Name"
-            placeholder="Last Name"
-            lazy-rules
-            :rules="[val => val && val.length > 0 || 'Please enter your last name']"
-        />
-        <q-input
-            filled
-            v-model="email"
-            label="Email"
-            placeholder="Email"
-            lazy-rules
-            :rules="[val => val && /.+@.+\..+/.test(val) || 'Please enter a valid email']"
-        />
-        <q-input
-            filled
-            v-model="BSN"
-            label="BSN"
-            placeholder="BSN"
-            lazy-rules
-            :rules="[val => val && val.length === 9 || 'Please enter a valid BSN']"
-        />
-        <q-input
-            filled
-            v-model="phone"
-            label="Phone Number"
-            placeholder="Phone"
-            lazy-rules
-            :rules="[val => val && val.length > 0 || 'Please enter your phone number']"
-        />
-        <q-input
-            filled
-            v-model="password"
-            label="Password"
-            lazy-rules
-            :rules="[val => val && val.length > 0 || 'Please enter your password', val => /[A-Z]/.test(val) || 'Password must contain at least one capital letter', val => /[^A-Za-z0-9]/.test(val) || 'Password must contain at least one special character']"
-        />
-
-
-        <div>
-          <q-btn  class="q-ml-auto" style="background: #f919a9; color: white" label="Add New User" type="submit" />
-        </div>
-      </q-form>
-    </div>
     <!-- ADD A BANK ACCOUNT -->
-        </q-tab-panel>
 
         <q-tab-panel name="newBankAccount">
           <div class="text-h6">Add new Bank Account</div>
