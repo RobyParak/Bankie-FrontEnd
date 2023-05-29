415da0a882956d635dea9ad976516e94b0f439de
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-05-29 07:46:27 +0200

    JWT front end

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -5,11 +5,8 @@
   </div>
   <div class="q-pa-md" style="display: flex; justify-content: center; align-items: center;">
     <div class="q-gutter-y-md column" style="max-width: 400px;">
-    <q-form
-        @submit="onSubmit"
-        class="q-gutter-md"
-    >
-      <q-input
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-input
           filled
           v-model="email"
           type="email"
@@ -17,40 +14,72 @@
           placeholder="Email"
           lazy-rules
           :rules="[val => val && /.+@.+\..+/.test(val) || 'Please enter a valid email']"
-      />
+        />
 
-      <q-input
+        <q-input
           filled
           type="password"
           v-model="password"
           label="Password *"
           lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Please enter your password' ]"
-      />
+          :rules="[val => val && val.length > 0 || 'Please enter your password']"
+        />
 
-      <div>
-        <q-btn  class="q-ml-auto" style="background: #f919a9; color: white" label="Login" type="submit" />
-      </div>
-    </q-form>
-
+        <div>
+          <q-btn class="q-ml-auto" style="background: #f919a9; color: white" label="Login" type="submit" />
+        </div>
+      </q-form>
+    </div>
   </div>
-  </div>
-
-  </template>
+</template>
 
 <script>
-import {QBtn} from "quasar";
+import { QBtn } from "quasar";
 export default {
-name: 'LoginButton',
-    components: {
-  QBtn
-},
-methods: {
-  login() {
-    // perform logout action here
+  name: 'LoginButton',
+  components: {
+    QBtn
+  },
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+  onSubmit() {
+    const loginData = {
+      email: this.email,
+      password: this.password
+    };
+
+    // Send the login request to your backend API
+    // and handle the response to obtain the JWT token
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Assuming the JWT token is returned in the response as 'token'
+        const token = data.token;
+
+        // Store the token in local storage or another suitable storage method
+        localStorage.setItem('token', token);
+
+        this.$router.push('/UserDashboard'); // STILL NEED TO CHECK FOR ROLE AND REROUTE
+      })
+      .catch(error => {
+        // Handle the error
+        console.error('Login failed:', error);
+      });
   }
 }
-}
+};
 </script>
 
 <style scoped>
