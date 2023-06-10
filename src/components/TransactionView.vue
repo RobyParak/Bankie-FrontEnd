acd1fbe44327a3706c18237a429af42605110a2e
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-06-10 12:09:53 +0200

    two scripts - one works with the validation btn, one doesnt

--- src/components/TransactionView.vue
+++ src/components/TransactionView.vue
@@ -209,7 +209,224 @@ export default {
 };
 </script>
 
+<!-- version underneath work with btn but im not sure if it makes transactions correctly -->
+<!-- <script>
+import api from '../../axios.js'
+import { ref, } from "vue";
+import jwtDecode from "jwt-decode";
 
+
+export default {
+  name: "TransactionView.vue",
+  data() {
+    return {
+      user: {},
+      bankAccounts: [],
+      bankAccountFrom: '',
+      ATMSelection: '',
+      bankAccountTo: '',
+      ATMIban: "NL01INHO0000000001",
+      isFormValid: false,
+      isFormValidATM: false,
+      ibanValidationRule: [
+        (val) => !!val || "IBAN is required",
+        (val) => this.isValidIBAN(val) || "Invalid IBAN",
+      ],
+    };
+  },
+  mounted() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const email = decodedToken.sub;
+
+      api.getAccountByEmail(email)
+          .then(response => {
+            // Update the user data with the retrieved data
+            this.user = response.data[0];
+
+            const ownerId = this.user.id; // Move the ownerId assignment inside the callback
+
+            api.getBankAccounts(ownerId)
+                .then(response => {
+                  this.bankAccounts = response.data;
+
+                  // Populate bank account options
+                  this.bankAccountFromOption = this.bankAccounts.map(account => ({
+                    label: account.iban,
+                    value: account.type
+                  }));
+                })
+                .catch(error => {
+                  console.error('Error retrieving bank accounts:', error);
+                });
+          })
+          .catch(error => {
+            console.error('Error retrieving user data:', error);
+          });
+    }
+  },
+  setup() {
+    const text = ref('');
+    const amount = ref('');
+    const amountATM = ref('');
+    const tab = ref('normal_transaction');
+    const bankAccountFromOption = ref([]);
+    const radio = ref('withdraw');
+    const ATMOptions = ref([]);
+
+    return {
+      text,
+      amount,
+      tab,
+      bankAccountFromOption,
+      ATMOptions,
+      radio,
+      amountATM,
+    };
+  },
+  
+  methods: {
+    atmTransaction() {
+      const now = new Date();
+     // const options = {day: '2-digit', month: '2-digit', year: 'numeric'};
+      //TODO change back to the top option once back end supports it
+      const options = {hour: '2-digit', minute: '2-digit'};
+      const formattedTime = now.toLocaleTimeString([], options);
+
+      const transactionData = {
+        userPerforming: this.user.id,
+        amount: this.amountATM,
+        accountTo: "",
+        accountFrom: "",
+        time: formattedTime,
+        comment: "ATM Transaction",
+      };
+
+      if (this.radio === 'deposit') {
+        transactionData.accountFrom = this.ATMIban;
+        transactionData.accountTo = this.ATMSelection.value;
+      } else if (this.radio === 'withdraw') {
+        transactionData.accountFrom = this.ATMSelection.value;
+        transactionData.accountTo = this.ATMIban;
+      }
+
+      api.performTransaction(transactionData)
+          .then(response => {
+            const transactionResult = response.data;
+            console.log('Transaction performed successfully:', transactionResult);
+            this.$router.push('/SuccessfulTransaction');
+          })
+          .catch(error => {
+            console.error('Error performing transaction:', error);
+          });
+    },
+    populateBankAccountOptions() {
+      this.bankAccountFromOption = this.bankAccounts.map(account => ({
+        label: `${account.typeId === 1 ? 'Current' : 'Savings'} - ${account.iban} ${account.balance}€`,
+        value: account.iban
+      }));
+      this.ATMOptions = this.bankAccounts.map(account => ({
+        label: `${account.typeId === 1 ? 'Current' : 'Savings'} - ${account.iban} ${account.balance}€`,
+        value: account.iban
+      }));
+    },
+    async performTransactionWithValidation() {
+      const now = new Date();
+      // const options = {day: '2-digit', month: '2-digit', year: 'numeric'};
+      //TODO change back to the top option once back end supports it
+      const options = {hour: '2-digit', minute: '2-digit'};
+      const formattedTime = now.toLocaleTimeString([], options);
+
+      const transactionData = {
+        userPerforming: this.user.id,
+        accountFrom: this.bankAccountFrom.value,
+        accountTo: "",
+        amount: this.amount,
+        time: formattedTime,
+        comment: this.text,
+      };
+      transactionData.accountTo = this.bankAccountTo;
+
+      try {
+        // Get accountFrom details by iban
+        api.getBankAccountByIban(transactionData.accountFrom)
+            .then(response => {
+              // Update the user data with the retrieved data
+              this.bankAccountFrom = response.data[0];
+              // Get accountTo details by iban
+              api.getBankAccountByIban(this.bankAccountTo)
+                  .then(response => {
+                    // Update the user data with the retrieved data
+                    this.bankAccountTo = response.data[0];
+
+                    // Perform the validation checks
+                    const isSameOwner = this.bankAccountFrom.ownerId === this.bankAccountTo.ownerId;
+                    const isAccountFromActive = this.bankAccountFrom.statusId !== 1;
+                    const isAccountToActive = this.bankAccountTo.statusId !== 1;
+                    const isSameType = this.bankAccountFrom.typeId === this.bankAccountTo.typeId;
+
+                    // If accounts have the same owner and both are active, or if they have the same type regardless of the owner, proceed with the transaction
+                    if ((isSameOwner && isAccountFromActive && isAccountToActive) || (isSameType && this.bankAccountFrom.typeId === 1 && isAccountFromActive && isAccountToActive)) {
+                      api.performTransaction(transactionData)
+                          .then(response => {
+                            const transactionResult = response.data;
+                            console.log('Transaction performed successfully:', transactionResult);
+                          })
+                          .catch(error => {
+                            console.error('Error performing transaction:', error);
+                          });
+                    } else {
+                      console.log('Account validation failed. Transaction cannot be performed.');
+                    }
+                  })
+                  .catch(error => {
+                    console.error('Error retrieving accountTo data:', error);
+                  });
+            })
+            .catch(error => {
+              console.error('Error retrieving accountFrom data:', error);
+            });
+      } catch (error) {
+        console.error('Error performing transaction:', error);
+      }
+      this.$router.push('/SuccessfulTransaction');
+    },
+    isValidIBAN(iban) {
+      // Regular expression pattern to validate IBAN
+      const ibanPattern = /^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$/;
+      return ibanPattern.test(iban);
+    },
+    validateForm() {
+      // Validate the form fields and set isFormValid accordingly
+      this.isFormValid =
+          !!this.bankAccountFrom &&
+          !!this.bankAccountTo &&
+          !!this.amount;
+    },
+    validateFormATM(){
+      this.isFormValidATM =
+          !!this.ATMSelection &&
+          !!this.ATMIban &&
+          !!this.amountATM;
+    },
+  },
+  watch: {
+    bankAccountFrom() {
+      this.validateForm();
+    },
+    bankAccountTo() {
+      this.validateForm();
+    },
+    amount() {
+      this.validateForm();
+    },
+    amountATM() {
+      this.validateFormATM();
+    },
+  },
+};
+</script> -->
 
 <style scoped>
 /* Add your custom styles here */
