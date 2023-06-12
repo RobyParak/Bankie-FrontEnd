9ae4d41659ee9ca4bbe57d8104e2cc7f8daf4c7b
RobyParak <677676@student.inholland.nl>
2023-06-12 15:58:16 +0200

    Register user email address and other fields already in use

--- src/components/RegisterUser.vue
+++ src/components/RegisterUser.vue
@@ -6,57 +6,57 @@
   <div class="q-pa-md" style="justify-self: center; padding-left: 20em; max-width: 80%;">
     <q-form @reset="onReset" class="q-gutter-md">
       <q-input
-        filled
-        v-model="firstName"
-        label="First Name"
-        placeholder="First Name"
-        :rules="[val => val && val.length > 0 || 'Please enter your first name']"
-        ref="firstNameInput"
+          filled
+          v-model="firstName"
+          label="First Name"
+          placeholder="First Name"
+          :rules="[val => val && val.length > 0 || 'Please enter your first name']"
+          ref="firstNameInput"
       />
       <q-input
-        filled
-        v-model="lastName"
-        label="Last Name"
-        placeholder="Last Name"
-        :rules="[val => val && val.length > 0 || 'Please enter your last name']"
-        ref="lastNameInput"
+          filled
+          v-model="lastName"
+          label="Last Name"
+          placeholder="Last Name"
+          :rules="[val => val && val.length > 0 || 'Please enter your last name']"
+          ref="lastNameInput"
       />
       <q-input
-        filled
-        v-model="email"
-        label="Email"
-        placeholder="Email"
-        :rules="[val => val && /.+@.+\..+/.test(val) || 'Please enter a valid email']"
-        ref="emailInput"
+          filled
+          v-model="email"
+          label="Email"
+          placeholder="Email"
+          :rules="[val => val && /.+@.+\..+/.test(val) || 'Please enter a valid email']"
+          ref="emailInput"
       />
       <q-input
-        filled
-        v-model="bsn"
-        label="BSN"
-        placeholder="BSN"
-        :rules="[val => val && (val.length === 8 || val.length === 9) || 'Please enter a valid BSN']"
-        ref="bsnInput"
+          filled
+          v-model="bsn"
+          label="BSN"
+          placeholder="BSN"
+          :rules="[val => val && (val.length === 8 || val.length === 9) || 'Please enter a valid BSN']"
+          ref="bsnInput"
       />
 
       <q-input
-        filled
-        v-model="phoneNumber"
-        label="Phone Number"
-        placeholder="Phone"
-        :rules="[val => val && val.length > 0 || 'Please enter your phone number']"
-        ref="phoneNumberInput"
+          filled
+          v-model="phoneNumber"
+          label="Phone Number"
+          placeholder="Phone"
+          :rules="[val => val && val.length > 0 || 'Please enter your phone number']"
+          ref="phoneNumberInput"
       />
       <q-input
-        filled
-        v-model="password"
-        type="password"
-        label="Password"
-        :rules="[
+          filled
+          v-model="password"
+          type="password"
+          label="Password"
+          :rules="[
           val => val && val.length > 0 || 'Please enter your password',
           val => /[A-Z]/.test(val) || 'Password must contain at least one capital letter',
           val => /[^A-Za-z0-9]/.test(val) || 'Password must contain at least one special character'
         ]"
-        ref="passwordInput"
+          ref="passwordInput"
       />
 
       <div>
@@ -97,26 +97,15 @@ export default {
       }
 
       api
-        .createUser(userData)
-        .then(response => {
-          console.log(response.data)
-          this.$router.push('/success') // Redirect the user to the success page
-        })
-        .catch(error => {
-          // Check if the error is due to duplicate email address
-          if (
-            error.response &&
-            error.response.status === 400 &&
-            error.response.data.message === 'Email address already exists'
-          ) {
-            // Handle the duplicate email error
-            this.errorMessage = 'Email address already exists. Please choose a different email.'
-          } else {
-            // Handle other errors
-            console.error(error)
-            this.errorMessage = 'An error occurred. Please try again.'
-          }
-        })
+          .createUser(userData)
+          .then(response => {
+            console.log(response.data)
+            this.$router.push('/success') // Redirect the user to the success page
+          })
+          .catch(error => {
+            console.log(error);
+            this.errorMessage = error.response.data.message;
+          })
     },
   },
   setup() {
@@ -142,15 +131,15 @@ export default {
 
     const validateForm = () => {
       isFormValid.value =
-        !!firstName.value &&
-        !!lastName.value &&
-        !!email.value &&
-        /.+@.+\..+/.test(email.value) &&
-        (!!bsn.value && (bsn.value.length === 8 || bsn.value.length === 9)) &&
-        !!phoneNumber.value &&
-        !!password.value &&
-        /[A-Z]/.test(password.value) &&
-        /[^A-Za-z0-9]/.test(password.value)
+          !!firstName.value &&
+          !!lastName.value &&
+          !!email.value &&
+          /.+@.+\..+/.test(email.value) &&
+          (!!bsn.value && (bsn.value.length === 8 || bsn.value.length === 9)) &&
+          !!phoneNumber.value &&
+          !!password.value &&
+          /[A-Z]/.test(password.value) &&
+          /[^A-Za-z0-9]/.test(password.value)
     }
 
     return {
